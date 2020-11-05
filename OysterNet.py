6b708ed9b2b8608bf527bf88c75f3e67cf4273b0
mjauquet <72773507+mjauquet@users.noreply.github.com>
2020-11-05 14:33:24 -0500

    Updated layers of cnn

--- OysterNet.py
+++ OysterNet.py
@@ -8,16 +8,17 @@ class Net(nn.Module):
 
       # Creation of convolutional layers.
       # Define filters quantity, size, and type.
-      self.conv1 = nn.Conv2d(3, 6, 5)
-      self.pool = nn.MaxPool2d(5, 5) # Reduces the size of the filter image
-      self.conv2 = nn.Conv2d(6, 50, 5)
+      self.conv1 = nn.Conv2d(3, 10, 5)
+      self.pool = nn.MaxPool2d(4, 4) # Reduces the size of the filter image
+      self.conv2 = nn.Conv2d(10, 50, 5)
 
       # Creation of Linear layer.
-      # 
-      self.fc1 = nn.Linear(450, 120)
-      self.fc2 = nn.Linear(120, 84)
-      self.fc3 = nn.Linear(84, 10)
-      self.fc4 = nn.Linear(10, 3)
+      self.fc1 = nn.Linear(1250, 450)
+      self.fc2 = nn.Linear(450, 120)
+      self.fc3 = nn.Linear(120, 84)
+      self.fc4 = nn.Linear(84, 42)
+      self.fc5 = nn.Linear(42, 10)
+      self.fc6 = nn.Linear(10, 3)
 
     # x is our data (image)
     # Each line is passing our data through our defined layers above
@@ -27,13 +28,13 @@ class Net(nn.Module):
       # For each object in list, classify that object
         # x = self. fucntion that classifies() things below
       x = self.pool(F.relu(self.conv1(x)))      # Passing x through first layer
-      #print(x)
       x = self.pool(F.relu(self.conv2(x)))
-      #print(x)
-      x = x.view(-1, 450)
+      x = x.view(-1, 1250)
       x = F.relu(self.fc1(x))
       x = F.relu(self.fc2(x))
       x = F.relu(self.fc3(x))
-      x = self.fc4(x)
+      x = F.relu(self.fc4(x))      
+      x = F.relu(self.fc5(x))
+      x = self.fc6(x)
       
       return x
