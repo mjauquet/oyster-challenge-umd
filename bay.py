78efa2b706b5b118fab431f89f99fc90b1c38e83
dcastillo40 <43612889+dcastillo40@users.noreply.github.com>
2020-11-18 17:14:27 -0500

    Add files via upload
    updated version - changed dataset

--- bay.py
+++ bay.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torchvision.transforms as T
 import pandas as pd
+from skimage import io, transform
 
 from torch.utils.data import Dataset
 from PIL import Image
@@ -22,6 +23,8 @@ from engine import train_one_epoch, evaluate
 import utils
 import importlib
 import os
+
+
 
 # Network
 class Net(nn.Module):
@@ -57,9 +60,9 @@ class Net(nn.Module):
 
 # Dataset
 class OysterDataset(Dataset):
-    ''' Dataset for crops of oyster images '''
+    ''' Dataset for detection of oyster images '''
 
-    def __init__(self, root_dir, csv_file, transform=None):
+    def __init__(self, root_dir, csv_file, transforms):
         '''
         Initialize variable for data set
         root_dir (string) is the directory with all the images
@@ -69,10 +72,8 @@ class OysterDataset(Dataset):
 
         self.oysters = pd.read_csv(csv_file)
         self.root_dir = root_dir
-        self.transform = transform
+        self.transforms = transforms
 
-    def __len__(self):
-        return len(self.oysters)
 
     def __getitem__(self, index):
         if torch.is_tensor(index):
@@ -80,50 +81,52 @@ class OysterDataset(Dataset):
 
         # Get path of image which is row[index], col[0]
         img_name = os.path.join(self.root_dir, self.oysters.iloc[index, 0])
-        
-        # Following Face landmark dataset
-        # Load image
-        #image = io.imread(img_name)
 
         # following PedFundanDataset
         img = Image.open(img_name).convert("RGB")
-        
-        img = np.array(img)
 
-        if self.transform is not None:
-            img = self.transform(img)
-
-        # Get class for image row[index], col[4]
-        target = self.oysters.iloc[index, 4]
+        # Getting Labels
+        num_objs = self.oysters.iloc[index, 4]
+        print("There are ", num_objs, "objects in ", img_name)
         
+        labels = []
+        boxes = []
+
+        for i in range(num_objs):
+            pos = 8 + i * 5     # get column for object bounding boxes. boxes start at column 8
+            xmin  = self.oysters.iloc[index, pos + 1]
+            ymin = self.oysters.iloc[index, pos + 2]
+            xmax = self.oysters.iloc[index, pos + 3]
+            ymax = self.oysters.iloc[index, pos + 4]
+            boxes.append([xmin, ymin, xmax, ymax])
+
+            obj_class = self.oysters.iloc[index, pos]   # get class of object 0,1, or 2
+            labels.append(1)
+
+            #print("Box: ", boxes[i], "\nClass: ", labels[i])
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        #print(boxes)
+        
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        #print(labels)                
+
+        img_id = torch.tensor([index])    # name of image
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]) # area of image
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["imgage_id"] = img_id
+        target["area"] = area
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+
         return img, target
 
-
-# main
-def run_file(pathName):
-  pathName += '/'
-
-  images = []
-
-  for image in os.listdir(pathName):
-    if(".jpg" in image): 
-      img_loc = plt.imread(pathName + image)
-
-      img_loc = np.array(img_loc)
-
-      img_loc = img_loc.flatten()
-
-      images.append(img_loc)
-
-  classes = ('alive', 'dead')
-
-  network = Net()
-
-  run_network(images, network)
-
-
-def run_network(images, network):
-  print("the")
+    def __len__(self):
+        return len(self.oysters)
 
 
 def get_model_instance_segmentation(num_classes):
@@ -142,90 +145,116 @@ def get_model_instance_segmentation(num_classes):
     return model
 
 def get_transform(train):
-  transform = []
-  transform.append(T.ToTensor())
+  transforms = []
+  transforms.append(T.ToTensor())
 
   if train:
-    transform.append(T.RandomHorizontalFlip(0.5))
+    transforms.append(T.RandomHorizontalFlip(0.5))
 
-  return T.Compose(transform)
+  return T.Compose(transforms)
 
 
 
-def main():
-  model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained = True)
+def main(): 
 
-  num_classes = 2
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True,
+                                                                 pretrained_backbone = False)
+    dataset = OysterDataset("/Users/darwin/Downloads/Bay project/Training",
+                          "/Users/darwin/Downloads/Bay project/Training/TrainingDetectionLabels3.csv",
+                          get_transform(train = True))
+    
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, num_workers=4,
+                                collate_fn=utils.collate_fn)
 
-  in_features = model.roi_heads.box_predictor.cls_score.in_features
+# For Training
+    images,targets = next(iter(data_loader))
 
-  model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    print(images)
+    
+    images = list(image for image in images)
+    targets = [{k: v for k, v in t.items()} for t in targets]
+    
+    output = model(images,targets)   # Returns losses and detections
+    
+# For inference
+    model.eval()
+    x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+    predictions = model(x)
 
-  backbone = torchvision.models.mobilenet_v2(pretrained = True).features
+    print(predictions)
 
-  backbone.out_channels = 1280
 
-  anchor_generator = AnchorGenerator(sizes = ((32,64, 128, 256, 512), ),
+
+
+
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained = True)
+
+    num_classes = 2
+
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    backbone = torchvision.models.mobilenet_v2(pretrained = True).features
+
+    backbone.out_channels = 1280
+
+    anchor_generator = AnchorGenerator(sizes = ((32,64, 128, 256, 512), ),
                                       aspect_ratios = ((0.5, 1.0, 2.0),))
 
-  roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names = [0], output_size = 7,
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names = [0], output_size = 7,
                                                   sampling_ratio= 2)
 
-  model = FasterRCNN(backbone, num_classes = 2, rpn_anchor_generator = anchor_generator,
+    model = FasterRCNN(backbone, num_classes = 2, rpn_anchor_generator = anchor_generator,
                      box_roi_pool = roi_pooler)
 
   
   
   # Train classifier
-  device = torch.device('cpu')
+    device = torch.device('cpu')
 
-  dataset = OysterDataset("/Users/darwin/Downloads/Testing",
-                          "/Users/darwin/Downloads/Testing/P1010129.csv",
+    dataset = OysterDataset("/Users/darwin/Downloads/Bay project/Training",
+                          "/Users/darwin/Downloads/Bay project/Training/TrainingDetectionLabels3.csv",
                           get_transform(train = True))
 
-  dataset_test = OysterDataset("/Users/darwin/Downloads/Testing",
-                              "/Users/darwin/Downloads/Testing/P1010129.csv",
+    dataset_test = OysterDataset("/Users/darwin/Downloads/Bay project/Training",
+                              "/Users/darwin/Downloads/Bay project/Training/TrainingDetectionLabels3.csv",
                              get_transform(train = False))
 
-  indices = torch.randperm(len(dataset)).tolist()
-  dataset = torch.utils.data.Subset(dataset, indices[:-50])
-  dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
-  data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle = False, num_workers=4,
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=3, shuffle = False, num_workers=4,
                                             collate_fn = utils.collate_fn)
 
-  data_loader_test = torch.utils.data.DataLoader(dataset_test, batch_size=2, shuffle = False, num_workers=4,
+    data_loader_test = torch.utils.data.DataLoader(dataset_test, batch_size=3, shuffle = False, num_workers=4,
                                                   collate_fn=utils.collate_fn)
 
-  model = get_model_instance_segmentation(num_classes)
+    model = get_model_instance_segmentation(num_classes)
 
-  model.to(device)
+    model.to(device)
 
-  params = [p for p in model.parameters() if p.requires_grad]
-  optimizer = torch.optim.SGD(params, lr=0.005,
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
                                 momentum=0.8, weight_decay=0.0005)
 
-  lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                                   step_size=3,
-                                                   gamma=0.1)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
+    num_epochs = 1
 
-  num_epochs = 10
+    for epoch in range(num_epochs): 
+        train_one_epoch(model, optimizer, data_loader_test, device, epoch, print_freq=10)
 
-  for epoch in range(num_epochs):
-    print(epoch);
-      
-    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        print(epoch);
 
-    print(epoch);
+        lr_scheduler.step()
 
-    lr_scheduler.step()
-
-    evaluate(model, data_loader_test, device=device)
+        evaluate(model, data_loader_test, device=device)
 
     
 
-  print("That's it!")
+    print("That's it!")
 
 
 
