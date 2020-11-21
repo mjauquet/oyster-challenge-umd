a628797eca87db18329967096cb6130863d5c6c7
mjauquet <72773507+mjauquet@users.noreply.github.com>
2020-11-21 15:33:57 -0500

    Working script on colab

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ import sys
 import os
 
 import torch
+import torchvision
+import torchvision.transforms as T
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+from PIL import Image
 
 
 # Main function
@@ -22,11 +27,10 @@ def main(self):
         image_path = sys.argv[1]
 
         if not os.path.exists(image_path):
-            print('File path does not exist')
+            print('Error: File path does not exist')
         else:
             # Call function to run model here
-            # 
-            print(' ')
+            runModel(image_path)
     else:
         print('Error: Command should have the form:')
         print('app.exe <Input File Path>')
@@ -34,18 +38,60 @@ def main(self):
 
 def runModel(filepath):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # Open image and convert to PIL image
+    img = get_img_as_tensor(filepath)
+
     # get model 
-    # num_classes = 4
-    # model = get_model_instance_segmentation(num_classes)
-    # model.eval()
+    num_classes = 4
+    model = get_model(num_classes)
+    model.eval()
 
     # Run model
-    # with torch.no_grad()
-    #   prediction = model([img.to(device)])
+    with torch.no_grad():
+      prediction = model([img.to(device)])
 
     # print output array
-    # num_live = (prediction[0]['labels'] == 1).sum(dim=0).item()
-    # num_dead = (prediction[0]['labels'] == 2).sum(dim=0).item()
-    # total_oysters = num_live + num_dead
-    # output = [total_oysters, num_live, num_dead]
-    # print("Predicted: ", output)
+    num_live = (prediction[0]['labels'] == 1).sum(dim=0).item()
+    num_dead = (prediction[0]['labels'] == 2).sum(dim=0).item()
+    total_oysters = num_live + num_dead
+    output = [total_oysters, num_live, num_dead]
+    print(output)
+
+# Returns Model to be used for detection
+def get_model(num_classes):
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained = True)
+
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    hidden_layer = 256
+
+    PATH = 'detection_net.pth'
+    model.load_state_dict(torch.load(PATH))
+
+    return model
+
+# Return image as transform so it can be run through model
+def get_img_as_tensor(filepath):
+    # Open image as PIL image
+    img = Image.open(filepath).convert("RGB")
+
+    # get transform
+    image_transform = get_transform()
+
+    # transform image 
+    img = image_transform(img)
+
+    return img
+
+# Return transform for image
+def get_transform():
+  transforms = []
+  transforms.append(T.ToTensor())
+
+  return T.Compose(transforms)
+
+if __name__ == "__main__":
+    main(sys.argv)
