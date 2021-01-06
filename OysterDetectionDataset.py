89d1abcc67435e55c8dc284050083a6937af9782
mjauquet <72773507+mjauquet@users.noreply.github.com>
2021-01-06 16:59:16 -0500

    Cleaning repo

--- OysterDetectionDataset.py
+++ OysterDetectionDataset.py
@@ -1,3 +1,6 @@
+# Dataset for training
+# Author: Martin Jauquet and Darwin Castillo
+# This class is used to open the csv of the dataset and open the image and aquire the associated labels with each image. 
 import os
 import torch
 from torch.utils.data import Dataset
@@ -34,45 +37,48 @@ class OysterDetectionDataset(Dataset):
 
         # following PedFundanDataset
         img = Image.open(img_name).convert("RGB")
-        img = np.array(img)
+        #img = np.array(img)
+        mask = Image.open(img_name)
+        mask = np.array(mask)
 
         # Getting Labels
         num_objs = self.oysters.iloc[index, 4]
-        print("There are ", num_objs, "objects in ", img_name)
+        #print("There are ", num_objs, "objects in ", img_name)
         labels = []
         boxes = []
 
-        if num_objs == 0:
-            boxes.append([0,0,0,0])
-        else:
-            for i in range(num_objs):
-                pos = 8 + i * 5     # get column for object bounding boxes. boxes start at column 8
-                xmin  = self.oysters.iloc[index, pos + 1]
-                ymin = self.oysters.iloc[index, pos + 2]
-                xmax = self.oysters.iloc[index, pos + 3]
-                ymax = self.oysters.iloc[index, pos + 4]
-                boxes.append([xmin, ymin, xmax, ymax])
-
-                obj_class = self.oysters.iloc[index, pos]   # get class of object 0,1, or 2
-                labels.append(obj_class)
-
-                print("Box: ", boxes[i], "\nClass: ", labels[i])
+       
+        for i in range(num_objs):
+          pos = 8 + i * 5     # get column for object bounding boxes. boxes start at column 8
+          xmin  = self.oysters.iloc[index, pos + 1]
+          ymin = self.oysters.iloc[index, pos + 2]
+          xmax = self.oysters.iloc[index, pos + 3]
+          ymax = self.oysters.iloc[index, pos + 4]
+          boxes.append([xmin, ymin, xmax, ymax])
+         
+          obj_class = self.oysters.iloc[index, pos]   # get class of object 0,1, or 2
+          labels.append(obj_class)
+         
+          #print("Box: ", boxes[i], "\nClass: ", labels[i])
 
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        print(boxes)
+        #print(boxes)
         
         labels = torch.as_tensor(labels, dtype=torch.int64)
-        print(labels)                
+        #print(labels)                
 
-        img_id = self.oysters.iloc[index, 0]    # name of image
+        img_id = torch.as_tensor([index])   # name of image
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]) # area of image
-        print(area)
+        #print(area)
+
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
         target = {}
         target["boxes"] = boxes
         target["labels"] = labels
-        target["imgage_id"] = img_id
+        target["image_id"] = img_id
         target["area"] = area
+        target["iscrowd"] = iscrowd
 
         if self.transform is not None:
             img = self.transform(img)
